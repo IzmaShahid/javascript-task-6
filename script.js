710eef21eeccb1c3e053fddf388114621bc14eda
IzmaShahid <pss.10807.izma@gmail.com>
2024-09-28 20:29:52 +0500

    javascript task6

---
+++ script.js
@@ -0,0 +1,9 @@
+function changecolor(){
+   let color = `rgb(${Math.floor(Math.random() * 255) + 1},${Math.floor(Math.random() * 255) + 1},${Math.floor(Math.random() * 255) + 1})`;
+   document.body.style.backgroundColor = color ;
+//    function colorchange() {
+//     let bg = `rgb(${Math.floor(Math.random() * 255) + 1}, 
+//     ${Math.floor(Math.random() * 255) + 1} ,${Math.floor(Math.random() * 255) + 1})`;
+//     document.body.style.backgroundColor = bg;
+}
+
